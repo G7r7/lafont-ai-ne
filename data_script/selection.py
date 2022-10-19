b33086f173ccb11a9b4420985a93eabe1067e40c
loic <loic.senecat@bsd.fr>
2022-10-19 20:24:18 +0200

    add poems selection

---
+++ data_script/selection.py
@@ -0,0 +1,16 @@
+﻿import re
+
+def poem_selection(path, filename):
+  with open(path,'r') as file_stream:
+    text = file_stream.read()
+    paragraphes = re.split(r"\n{2,}", text)
+
+    poems = [paragraphe for paragraphe in paragraphes if paragraphe.count('\n') > 1]
+
+    id = 0
+    for poem in poems:
+      with open('poems/' + filename + str(id) + '.txt','wb') as poem_stream:
+        poem_stream.write(poem.encode())
+      id += 1
+
+  return 0
